1a60cfb601fcb6c7b7d64407e7838eea17163175
Andrew Quaye <andyquayegh@outlook.com>
2018-10-21 16:35:34 +0100

    add token
    add token

--- index.php
+++ index.php
@@ -8,8 +8,12 @@
 header("Content-Type: Application/JSON");
 $_json = new stdClass();
 
-if ($_GET['api'] ==="sms"){
+if ($_REQUEST['api'] ==="sms") {
     require_once "sms/sms.php";
+}elseif($_REQUEST['api'] === "sms-token"){
+    $toke = uniqid();
+    $_json->Token = $toke;
+    echo json_encode($_json);
 }else{
     $_json->error = "404";
     $_json->msg ="Missing function";
