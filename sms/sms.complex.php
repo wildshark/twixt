afd1205a04d9c45afc631855b871bbc2ef65d819
Andrew Quaye <andyquayegh@outlook.com>
2018-10-21 18:44:39 +0100

    file sms.complex .php
    add file sms.complex .php

--- sms/sms.complex.php
+++ sms/sms.complex.php
@@ -6,4 +6,18 @@
  * Time: 6:32 PM
  */
 
-echo $token = $_REQUEST['token'];
+include_once "sms/config.php";
+
+if ($_SESSION['token'] === $_REQUEST['token']){
+   $to = $_REQUEST['to'];
+   $from = $_REQUEST['from'];
+   $msg = $_REQUEST['msg'];
+   $success = $_REQUEST['success'];
+   $fail = $_REQUEST['fail'];
+
+}else{
+    $_json->Error = "2200";
+    $_json->msg = "Missing token";
+    $my_json_output = json_encode($_json);
+    echo $my_json_output;
+};
