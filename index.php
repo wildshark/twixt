2ac769ec9e61ba96f1c52e6f4d9eaa8fad1a2283
Andrew Quaye <andyquayegh@outlook.com>
2018-10-21 18:36:39 +0100

    add sms complex
    add token
    add success - url
    add fail url

--- index.php
+++ index.php
@@ -7,13 +7,17 @@
  */
 header("Content-Type: Application/JSON");
 $_json = new stdClass();
+session_start();
 
 if ($_REQUEST['api'] ==="sms") {
     require_once "sms/sms.php";
-}elseif($_REQUEST['api'] === "sms-token"){
-    $toke = uniqid();
+}elseif($_REQUEST['api'] === "sms-token") {
+    $token = uniqid();
+    $_SERVER['token'] = $token;
     $_json->Token = $toke;
     echo json_encode($_json);
+}elseif ($_REQUEST['api'] ==='sms-complex'){
+    require_once "sms/sms.complex.php";
 }else{
     $_json->error = "404";
     $_json->msg ="Missing function";
---
+++ sms/sms.complex.php
@@ -0,0 +1,9 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Andrew Quaye
+ * Date: 21/10/2018
+ * Time: 6:32 PM
+ */
+
+echo $token = $_REQUEST['token'];
